052a27195dacdc4b17f3a76756eb09b36e450133
Joaovitorvas <joaovitorvasc.2307@outlook.com>
2022-10-01 16:19:23 -0300

    Operadores

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores {
+        public static void main(String[] args) {
+            int A = 5;
+            int B = 3;
+    
+            int Soma = A + B;
+            int Subtração = A - B;
+            int Divisão = A / B;
+            int Multiplicacao = A * B;
+            int Modulo = A / B;
+            int Potencia = A / B;
+    
+            System.out.println("Soma = " + Soma);
+            System.out.println("Subtração = " + Subtração);
+            System.out.println("Divisão = " + Divisão);
+            System.out.println("Multiplicação = " + Multiplicacao);
+            System.out.println("Modulo = " + Modulo);
+            System.out.println("Potência = " + Potencia);
+        }
+    }
---
+++ tipos-e-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,8 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        Double PI = 3.14;
+
+        System.out.println(PI);
+
+    }
+}
