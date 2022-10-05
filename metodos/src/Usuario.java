313900bd059390f1f0b48628136ef5af65ea9484
Joaovitorvas <joaovitorvasc.2307@outlook.com>
2022-10-05 11:17:47 -0300

    Metodos

---
+++ metodos/src/Usuario.java
@@ -0,0 +1,22 @@
+public class Usuario {
+    public static void main(String[] args) {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        smartTv.mudarCanal(13);
+
+        System.out.println("TV Ligada: " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("TV Ligada: " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("TV Ligada: " + smartTv.ligada);
+    }
+}
