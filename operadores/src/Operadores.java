4f560c61dc57cbc2ca3c34153ff2c899a819bae3
Joaovitorvas <joaovitorvasc.2307@outlook.com>
2022-10-04 21:21:20 -0300

    Sim

--- operadores/src/Operadores.java
+++ operadores/src/Operadores.java
@@ -1,20 +1,84 @@
 public class Operadores {
+        public Operadores() {
+    }
+
         public static void main(String[] args) {
-            int A = 5;
-            int B = 3;
+            int a = 6;
+            int b = 5;
+
+            String resultado = a == b ? "verdadeiro" : "falso";
+
+            System.out.println(resultado);
+            
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            // int A = 5;
+            // int B = 3;
     
-            int Soma = A + B;
-            int Subtração = A - B;
-            int Divisão = A / B;
-            int Multiplicacao = A * B;
-            int Modulo = A / B;
-            int Potencia = A / B;
+            // int Soma = A + B;
+            // int Subtração = A - B;
+            // double Divisão = A / B;
+            // int Multiplicacao = A * B;
+            // int Modulo = A / B;
+            // int Potencia = A / B;
     
-            System.out.println("Soma = " + Soma);
-            System.out.println("Subtração = " + Subtração);
-            System.out.println("Divisão = " + Divisão);
-            System.out.println("Multiplicação = " + Multiplicacao);
-            System.out.println("Modulo = " + Modulo);
-            System.out.println("Potência = " + Potencia);
+            // System.out.println("Soma = " + Soma);
+            // System.out.println("Subtração = " + Subtração);
+            // System.out.println("Divisão = " + Divisão);
+            // System.out.println("Multiplicação = " + Multiplicacao);
+            // System.out.println("Modulo = " + Modulo);
+            // System.out.println("Potência = " + Potencia);
+
+
+
+            // int numero = 5;
+
+            // numero = - numero;
+
+            // System.out.println(- numero);
+
+            // System.out.println(numero);
         }
     }
